bbb042d5fd73c9c75f1ba37b1bb7d07d08e7d7ad
KNR1997 <kethakaranasinghe@gmail.com>
2023-08-16 13:16:25 +0530

    ELMS: first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzUPOLeeNCPrDKBr7MA5sAH7fYaa2GlEY",
+  authDomain: "e-learning-management-sy-befbb.firebaseapp.com",
+  projectId: "e-learning-management-sy-befbb",
+  storageBucket: "e-learning-management-sy-befbb.appspot.com",
+  messagingSenderId: "1058591115941",
+  appId: "1:1058591115941:web:a70913074fa6ea14759b4c"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from "react";
+import "./login.scss";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassowrd] = useState("");
+
+  const navigate = useNavigate()
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate("/")
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassowrd(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && <span>Wrong email or password!</span>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
